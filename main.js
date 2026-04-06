dfc6c71bc20ec34fc24f737f75d3763a0327618a
Fahd AHSAYNI <fahdahsayni@gmail.com>
2026-04-06 21:06:08 +0100

    feat: add modulo and factorial functions to main.js

--- main.js
+++ main.js
@@ -18,8 +18,18 @@ function pow(a, b) {
     return Math.pow(a, b);
 }
 
+function mod(a, b) {
+    return a % b;
+}
+
+function fact(a) {
+    return a * fact(a - 1);
+}
+
 console.log(sum(1, 2));
 console.log(sub(1, 2));
 console.log(mul(1, 2));
 console.log(div(1, 2));
 console.log(pow(1, 2));
+console.log(mod(1, 2));
+console.log(fact(1));
